ae47d324de7c01ca47fa69b3e833ada325f29869
kmaylor <kmaylor@ucdavis.edu>
2018-11-04 22:37:15 -0800

    testing by building cardiac model

--- denoiser.py
+++ denoiser.py
@@ -1,14 +1,14 @@
 import numpy as np
 import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
+#matplotlib.use('Agg')
+#import matplotlib.pyplot as plt
 import pickle as pk
 from os.path import exists
 from os import makedirs
 import types
 from keras.models import Sequential, load_model, model_from_json
 from keras.layers import Dense, Activation, Flatten, Reshape
-from keras.layers import Conv1D, Conv2D, Cropping2D
+from keras.layers import Conv1D, Conv2D, Cropping2D, Cropping1D, UpSampling1D
 from keras.layers import LeakyReLU, Dropout, Lambda
 from keras.optimizers import Adam
 from keras.backend import log, count_params, int_shape
@@ -24,8 +24,8 @@ class DeNoiser(object):
                        channel=1,
                        min_features=64,
                        drop_rate=[.4,.4],
-                       kernels = [6,4,2],
-                       strides = [2,2,2],
+                       kernels = [6,6,6,4,4],
+                       strides = [2,2,2,2,2],
                        feature_scale = None,
                        ):
 
@@ -33,8 +33,8 @@ class DeNoiser(object):
         self.channel = channel
         self.E = None   # encoder
         self.D = None   # decoder
-        self.DN = None  # denoiser model
-        self.n_feautures = max_features
+        self.ED = None  # denoiser model
+        self.n_features = min_features
         self.e_drop_r = drop_rate[0]
         self.d_drop_r = drop_rate[1]
         self.kernels = kernels
@@ -46,20 +46,20 @@ class DeNoiser(object):
             self.feature_scale = feature_scale()
         else:
             self.feature_scale = feature_scale
-        self.Conv = set_convolution(dims)
-        self.UpS = set_upsample(dims)
+        self.Conv = self.set_convolution(dims)
+        self.UpS = self.set_upsample(dims)
     
-    def set_convolution(dims):
+    def set_convolution(self,dims):
         d = len(dims)
         if d == 1:
             return Conv1D
         elif d == 2:
             return Conv2D
             
-    def set_upsample(dims):
+    def set_upsample(self,dims):
         d = len(dims)
         if d == 1:
-            return need to do
+            return UpSampling1D
         elif d == 2:
             return self.UpSampling2DBilinear
     
@@ -69,8 +69,8 @@ class DeNoiser(object):
         self.E = Sequential(name='encoder')
         n_features = self.n_features
         feature_scale = self.feature_scale
-        dropout = self.edrop_r
-        input_shape = self.dims.extend(self.channel)
+        dropout = self.e_drop_r
+        input_shape = list(self.dims)+[self.channel]
         self.E.add(self.Conv(n_features*feature_scale[0], self.kernels[0], strides=self.strides[1], input_shape=input_shape,\
                 padding='same',name='E_con_1'))
         self.E.add(LeakyReLU(alpha=0.2,name='leak_1'))
@@ -89,36 +89,35 @@ class DeNoiser(object):
         self.D = Sequential(name='decoder')
         n_features = self.n_features
         feature_scale = self.feature_scale[::-1]
-        dropout = self.ddrop_r
-        input_shape = (self.length, self.channel)
-        upsample_layer = self.UpSampling2DBilinear
-        self.D.add(self.Conv(features*feature_scale[0], self.kernels[-1], strides=1, input_shape=input_shape,\
-                padding='same',name='D_con_1'))
+        dropout = self.d_drop_r
+        input_shape = self.encoder().layers[-1].output_shape[1:]
+        self.D.add(self.Conv(n_features*feature_scale[0], self.kernels[-1], strides=1, input_shape=input_shape,\
+            padding='same',name='D_con_1'))
         self.D.add(self.UpS(self.strides[-1],name='UpS_1'))
         self.D.add(LeakyReLU(alpha=0.2,name='leak_1'))
         for i,ks in enumerate(zip(self.kernels[::-1][1:],self.strides[::-1][1:])):
-            self.D.add(self.Conv(features*feature_scale[i+1], ks[0], strides=1, input_shape=input_shape,\
-                padding='same',name='con_%i'%(i+2)))
-            self.D.add(self.UpS(ks[1],name='UpS_%i'%(i+2))))
-            if i+1 == len(self.kernels):
-                self.D.add(Activation('tanh', name = 'Tanh'))
-            else:
-                self.D.add(LeakyReLU(alpha=0.2,name='leak_%i'%(i+2)))
+            if i+1 == len(self.kernels): break
+            self.D.add(self.Conv(n_features*feature_scale[i+1], ks[0], strides=1,    padding='same',name='D_con_%i'%(i+2)))
+            self.D.add(self.UpS(ks[1],name='UpS_%i'%(i+2)))
+            self.D.add(LeakyReLU(alpha=0.2,name='leak_%i'%(i+2)))
             self.D.add(Dropout(dropout,name='drop_%i'%(i+2)))
+        self.D.add(self.Conv(1, self.kernels[-1], strides=1, padding='same',name='D_con_%i'%(i+3)))
+        self.D.add(Activation('tanh', name = 'Tanh'))
+        self.D.add(Cropping1D(cropping=(8,8), name='Crop'))
         self.D.summary()
         return self.D
     
     def denoiser_model(self):
-        if self.AE:
-            return self.AE
-        optimizer = Adam(lr=0.0002,beta_1=0.5, decay=0)
-        self.AE = Sequential(name='aemodel')
-        self.AE.add(self.encoder())
-        self.AE.add(self.decoder())
-        self.AE.compile(loss='mean_squared_error', optimizer=optimizer,\
+        if self.ED:
+            return self.ED
+        optimizer = Adam(lr=0.002,beta_1=0.9, decay=0)
+        self.ED = Sequential(name='EDmodel')
+        self.ED.add(self.encoder())
+        self.ED.add(self.decoder())
+        self.ED.compile(loss='mean_squared_error', optimizer=optimizer,\
             metrics=['accuracy'])
-        self.AE.summary()
-        return self.AE
+        self.ED.summary()
+        return self.ED
         
     def UpSampling2DBilinear(self,stride, **kwargs):
             def layer(x):
